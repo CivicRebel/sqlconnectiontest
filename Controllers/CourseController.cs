12a0a87b83c165baf0b5f74bb66eecf934f7c868
Milcu Mihai <milcu.cristian.q7f@student.ucv.ro>
2023-08-20 17:20:47 +0300

    Add project files.

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sqlconnectiontest.Models;
+using sqlconnectiontest.Services;
+
+namespace sqlconnectiontest.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseService _course_service;
+        private readonly IConfiguration _configuration;
+
+        public CourseController(CourseService _svc, IConfiguration config)
+        {
+            _course_service = _svc;
+            _configuration = config;
+        }
+
+        // The Index method is used to get a list of courses and return it to the view
+        public IActionResult Index()
+        {
+            IEnumerable<Course> _course_list = _course_service.GetCourses(_configuration.GetConnectionString("SQLConnection"));
+            return View(_course_list);
+        }
+    }
+}
