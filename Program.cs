28d97410ed96b0045428c4de415e0da366b5c288
DESKTOP-7JOJJPV\milcu <aa>
2023-08-24 01:03:56 +0300

    fix

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc();
 builder.Services.AddTransient<CourseService>();
 builder.Services.AddFeatureManagement();
-builder.Configuration.AddAzureAppConfiguration(options => { options.Connect("Endpoint=https://appconfigsampleapp.azconfig.io;Id=p+QX;Secret=na8OFwE0rl6fW+y/7jAUK65l8UqkgqbKpJkEBcGekF0=")
-    .UseFeatureFlags()});
+builder.Configuration.AddAzureAppConfiguration(options => {
+    options.Connect("Endpoint=https://appconfigsampleapp.azconfig.io;Id=p+QX;Secret=na8OFwE0rl6fW+y/7jAUK65l8UqkgqbKpJkEBcGekF0=")
+    .UseFeatureFlags();
+});
 var app = builder.Build();
 
 app.UseRouting();
