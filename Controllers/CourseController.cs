c9c1146a0e3e2d849da7f51762d61d50d4bc6ac2
DESKTOP-7JOJJPV\milcu <aa>
2023-08-24 00:23:42 +0300

    Async feature

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement;
 using sqlconnectiontest.Models;
 using sqlconnectiontest.Services;
 
@@ -8,19 +9,29 @@ namespace sqlconnectiontest.Controllers
     {
         private readonly CourseService _course_service;
         private readonly IConfiguration _configuration;
+        private readonly IFeatureManager _featureManager;
 
-        public CourseController(CourseService _svc, IConfiguration config)
+        public CourseController(CourseService _svc, IConfiguration config, IFeatureManager featureManager)
         {
             _course_service = _svc;
             _configuration = config;
+            _featureManager = featureManager;
         }
 
         // The Index method is used to get a list of courses and return it to the view
         public IActionResult Index()
         {
-            ViewData["abc"] = _configuration["sampleapp:testkey"];
+            Abc();
             IEnumerable<Course> _course_list = _course_service.GetCourses(_configuration.GetConnectionString("SQLConnection"));
             return View(_course_list);
+        }
+
+        public async void Abc()
+        {
+            if (await _featureManager.IsEnabledAsync("enableFeature"))
+            {
+                ViewData["abc"] = _configuration["sampleapp:testkey"];
+            }
         }
     }
 }
