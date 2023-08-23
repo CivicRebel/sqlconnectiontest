7829a7c5222637e490bd14fae9183ffb44cdbaea
DESKTOP-7JOJJPV\milcu <aa>
2023-08-24 00:52:43 +0300

    Attempt 1

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ using static System.Net.WebRequestMethods;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc();
 builder.Services.AddTransient<CourseService>();
-builder.Services.AddFeatureManagement();
 builder.Configuration.AddAzureAppConfiguration("Endpoint=https://appconfigsampleapp.azconfig.io;Id=p+QX;Secret=na8OFwE0rl6fW+y/7jAUK65l8UqkgqbKpJkEBcGekF0=");
+builder.Services.AddFeatureManagement();
 var app = builder.Build();
 
 app.UseRouting();
