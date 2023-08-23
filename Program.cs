4225ef377c2b039d39c40e7a0dcfca43b72b6969
DESKTOP-7JOJJPV\milcu <aa>
2023-08-23 21:37:24 +0300

    Added connection to appconfiguration

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.FeatureManagement;
 using sqlconnectiontest.Services;
+using static System.Net.WebRequestMethods;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddMvc();
 builder.Services.AddTransient<CourseService>();
+builder.Services.AddFeatureManagement();
+builder.Configuration.AddAzureAppConfiguration("Endpoint=https://appconfigsampleapp.azconfig.io;Id=p+QX;Secret=na8OFwE0rl6fW+y/7jAUK65l8UqkgqbKpJkEBcGekF0=");
 var app = builder.Build();
 
 app.UseRouting();
